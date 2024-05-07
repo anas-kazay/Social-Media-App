7a5d3ed381cbc2a9da50629c9783ba2f1a6bcf32
anas-kazay <anaskazay@gmail.com>
2024-05-07 19:51:45 +0100

    add backend

---
+++ backend/src/main/java/anas/kazay/backend/config/JwtConstant.java
@@ -0,0 +1,6 @@
+package anas.kazay.backend.config;
+
+public class JwtConstant {
+    public static final String SECRET_KEY="anas-kazay";
+    public static final String JWT_HEADER="Authorization";
+}
---
+++ backend/src/main/java/anas/kazay/backend/config/JwtProvider.java
@@ -0,0 +1,2 @@
+package anas.kazay.backend.config;public class JwtProvider {
+}
